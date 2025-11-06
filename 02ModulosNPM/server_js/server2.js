a77fd73f4e35a215e80788c2fd160f94d0a0cfff
https://github.com/DanielGM-34/2DAM_Programacion <dgarmen630@g.educaand.es>
2025-11-06 14:33:09 +0100

    actividad de node, npm y http hecha

---
+++ 02ModulosNPM/server_js/server2.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+function webServer(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hola Node.js</h1>');
+}
+
+http
+  .createServer(webServer)
+  .listen(3000, '127.0.0.1');
+
+console.log('Servidor corriendo en http://127.0.0.1:3000/');
