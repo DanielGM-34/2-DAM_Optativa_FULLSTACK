3115e7f81ba65a1b88832950422555f9332b0538
https://github.com/DanielGM-34/2DAM_Programacion <dgarmen630@g.educaand.es>
2025-11-17 09:01:02 +0100

    titulo dinámico con ejs

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('¡No sabes nada!')
+app.set('view engine', 'ejs')
+app.use('views', express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/pruebas', (req, res) => {
+  //console.log(__dirname) //ruta donde estamos
+  //res.send('Ya somos unos crack en Node+Express')
+  //pasa un json con un titulo
+  res.render('pruebas', { titulo: 'titulo nuevoooo' })
 })
 
 app.get('/contacto', (req, res) => {
   console.log(__dirname + '/public')
-  res.send('¡Estás en contactos!')
+  res.send('Estas en contactos')
 })
 
+
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/public/html/404.html")
+})
 
 app.listen(port, () => {
-    console.log(__dirname + '/public')
   console.log(`Example app listening on port ${port}`)
 })
+
